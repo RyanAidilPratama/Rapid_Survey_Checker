daa8c89dbafb3aa12b2f173023367467204191be
RyanPratama <221709679@stis.ac.id>
2020-03-10 11:19:48 +0700

    Refactoring MainActivity UI

--- app/src/main/java/com/android/id/rapidcheckerpkl59/activities/MainActivity.java
+++ app/src/main/java/com/android/id/rapidcheckerpkl59/activities/MainActivity.java
@@ -8,38 +8,45 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.id.rapidcheckerpkl59.R;
 import com.android.id.rapidcheckerpkl59.data.adapter.TeamMemberAdapter;
-import com.android.id.rapidcheckerpkl59.data.model.TeamMember;
 import com.android.id.rapidcheckerpkl59.data.viewmodel.TeamMemberViewModel;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
 
+    private boolean isFirstTime = true;
+    RecyclerView rvTeamMember;
+    ProgressBar progressBar;
+    ImageButton btnNextTeam, btnPrevTeam;
+    TextInputEditText idTeam;
+    TextInputLayout idCube;
+    TextView error;
+    TeamMemberViewModel teamMemberViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView rvAnggotatim = findViewById(R.id.list_team_member);
-        ProgressBar progressBar = findViewById(R.id.progress_bar);
-        rvAnggotatim.setHasFixedSize(true);
-        ImageButton btnSearch = findViewById(R.id.main_btn_search);
-        TextInputEditText idTim = findViewById(R.id.main_team_code_query);
-        TextInputLayout kotakId = findViewById(R.id.main_team_code_cube);
-        TextView error = findViewById(R.id.text_error);
-        TeamMemberViewModel teamMemberViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(TeamMemberViewModel.class);
-        idTim.addTextChangedListener(new TextWatcher() {
+        rvTeamMember = findViewById(R.id.list_team_member);
+        progressBar = findViewById(R.id.progress_bar);
+        rvTeamMember.setHasFixedSize(true);
+        btnNextTeam = findViewById(R.id.btn_next_team);
+        btnPrevTeam = findViewById(R.id.btn_prev_team);
+        idTeam = findViewById(R.id.main_team_code_query);
+        idTeam.setText("21001");
+        idCube = findViewById(R.id.main_team_code_cube);
+        error = findViewById(R.id.text_error);
+        teamMemberViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(TeamMemberViewModel.class);
+        idTeam.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -47,44 +54,87 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if (kotakId.getError() != null) {
-                    kotakId.setError(null);
+                if (idCube.getError() != null) {
+                    idCube.setError(null);
                 }
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
+                int id = Integer.parseInt(idTeam.getText().toString());
+                if (id > 21110) {
+                    idCube.setError("Maksimum 21110");
+                    Toast.makeText(MainActivity.this, "Kode tim hanya sampai 21110", Toast.LENGTH_SHORT).show();
+                } else if (id < 21001) {
+                    idCube.setError("Minimum 21001");
+                    Toast.makeText(MainActivity.this, "Kode tim hanya sampai 21001", Toast.LENGTH_SHORT).show();
+                } else {
+                    getTeamData();
+                }
 
             }
         });
-        btnSearch.setOnClickListener(view -> {
+        if (isFirstTime) {
+            getTeamData();
+            isFirstTime = false;
+        }
+        btnNextTeam.setOnClickListener(view -> {
             progressBar.setVisibility(View.VISIBLE);
             if (error.getVisibility() == View.VISIBLE) {
                 error.setVisibility(View.GONE);
             }
-            if (TextUtils.isEmpty(idTim.getText().toString())) {
-                kotakId.setError("Tidak boleh kosong!");
+            if (TextUtils.isEmpty(idTeam.getText())) {
+                idCube.setError("Tidak boleh kosong!");
                 progressBar.setVisibility(View.GONE);
             } else {
-                teamMemberViewModel.setTeamListMember(idTim.getText().toString());
-                teamMemberViewModel.getTeamMemberList().observe(this, new Observer<ArrayList<TeamMember>>() {
-                    @Override
-                    public void onChanged(ArrayList<TeamMember> teamMembers) {
-                        if (teamMembers != null) {
-                            rvAnggotatim.setVisibility(View.VISIBLE);
-                            rvAnggotatim.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-                            TeamMemberAdapter adapter = new TeamMemberAdapter(getApplicationContext());
-                            adapter.setListProvince(teamMembers);
-                            rvAnggotatim.setAdapter(adapter);
-                            progressBar.setVisibility(View.GONE);
-                        } else {
-                            progressBar.setVisibility(View.GONE);
-                            rvAnggotatim.setVisibility(View.GONE);
-                            error.setVisibility(View.VISIBLE);
-                        }
-                    }
-                });
+                int id = Integer.parseInt(idTeam.getText().toString()) + 1;
+                if (id < 21111) {
+                    idTeam.setText(String.valueOf(id));
+                    getTeamData();
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(this, "Kode Tim hanya sampai 21110", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+        btnPrevTeam.setOnClickListener(view -> {
+            progressBar.setVisibility(View.VISIBLE);
+            if (error.getVisibility() == View.VISIBLE) {
+                error.setVisibility(View.GONE);
+            }
+            if (TextUtils.isEmpty(idTeam.getText())) {
+                idCube.setError("Tidak Boleh Kosong");
+                progressBar.setVisibility(View.GONE);
+            } else {
+                int id = Integer.parseInt(idTeam.getText().toString()) - 1;
+                if (id > 21000) {
+                    idTeam.setText(String.valueOf(id));
+                    getTeamData();
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(this, "Kode Tim hanya sampai 21001", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    void getTeamData() {
+        if (!TextUtils.isEmpty(idTeam.getText())) {
+            teamMemberViewModel.setTeamListMember(idTeam.getText().toString());
+            teamMemberViewModel.getTeamMemberList().observe(this, teamMembers -> {
+                if (teamMembers != null) {
+                    rvTeamMember.setVisibility(View.VISIBLE);
+                    rvTeamMember.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                    TeamMemberAdapter adapter = new TeamMemberAdapter(getApplicationContext());
+                    adapter.setListProvince(teamMembers);
+                    rvTeamMember.setAdapter(adapter);
+                    progressBar.setVisibility(View.GONE);
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                    rvTeamMember.setVisibility(View.GONE);
+                    error.setVisibility(View.VISIBLE);
+                }
+            });
+        }
     }
 }
